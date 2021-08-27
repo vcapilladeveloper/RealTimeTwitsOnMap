7561468a8a7c1735b0095526a2813023eb07e96e
Victor Capilla <victorcapilladeveloper@gmail.com>
2021-08-27 10:48:01 +0200

    Clean project from unused files

--- RealTimeTwitsOnMap/RealTimeTwitsOnMapApp.swift
+++ RealTimeTwitsOnMap/RealTimeTwitsOnMapApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RealTimeTwitsOnMapApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
--- RealTimeTwitsOnMap/Views/MainView.swift
+++ RealTimeTwitsOnMap/Views/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  RealTimeTwitsOnMap
 //
 //  Created by Victor Capilla Developer on 27/8/21.
@@ -7,15 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
