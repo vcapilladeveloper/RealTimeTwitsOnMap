60279afe39f45b0f120b636f19f3f663c53a032e
Victor Capilla <victorcapilladeveloper@gmail.com>
2021-08-27 10:45:02 +0200

    Initial Commit

---
+++ RealTimeTwitsOnMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealTimeTwitsOnMap
+//
+//  Created by Victor Capilla Developer on 27/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealTimeTwitsOnMap/RealTimeTwitsOnMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeTwitsOnMapApp.swift
+//  RealTimeTwitsOnMap
+//
+//  Created by Victor Capilla Developer on 27/8/21.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeTwitsOnMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
